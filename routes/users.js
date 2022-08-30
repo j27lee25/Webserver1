c3bb828296ed89cf1b607cb3736e96a6fe5a20e8
jlee <jaeseungl@hotmail.com>
2022-08-30 18:44:36 -0400

    simplified the http methods

--- routes/users.js
+++ routes/users.js
@@ -1,25 +1,28 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 /** Note **/
 // Notice it is capital R on Router()
 // router.get(), router.post()...
 // All of route starts with 'users'. So app.get('/users/new') now can be router.get('/new')
 
-router.get("/", (req, res) => {
-  res.send("User List")
-})
+router
+  .route("/:id")
+  .get((req, res) => {
+    res.send(`Get user by id ${req.params.id}`);
+  })
+  .put((req, res) => {
+    res.send(`Update a user by id ${req.params.id}`);
+  })
+  .delete((req, res) => {
+    res.send(`Delete a user by id ${req.params.id}`);
+  })
+  .post((req, res) => {
+    res.send(`Create a new user`);
+  });
 
-router.get("/new", (req, res) => {
-  res.send("User New Form")
-})
+router.route("/new", (req, res) => {
+  res.send("User New Form");
+});
 
-router.post('/',  (req, res) => {
-  res.send("Create a user")
-})
-
-// access user by id
-router.get('/:id', (req, res) => {//access to the individual user. :id allows to access dynamically
-  res.send(`Get user by id ${req.params.id}`)
-})
-module.exports = router //export this router from this file, and import it to server.js
+module.exports = router; //export this router from this file, and import it to server.js
